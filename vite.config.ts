9a274544dd7ea4cd393a9102c4ebb5754960969c
Lautaro Araujo <lautaroarielaraujo@gmail.com>
2026-05-02 20:11:11 +0000

    use new ai generated logo instead

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,13 @@ export default defineConfig({
 		sveltekit(),
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
-			includeAssets: ['robots.txt', 'pwa-source.svg', 'apple-touch-icon-180x180.png'],
+			includeAssets: [
+				'robots.txt',
+				'favicon.ico',
+				'favicon-16x16.png',
+				'favicon-32x32.png',
+				'apple-touch-icon.png'
+			],
 			manifest: {
 				name: 'Lautronome',
 				short_name: 'Lautronome',
@@ -20,11 +26,10 @@ export default defineConfig({
 				orientation: 'portrait',
 				categories: ['music', 'productivity', 'utilities'],
 				icons: [
-					{ src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
-					{ src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-					{ src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+					{ src: 'android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+					{ src: 'android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
 					{
-						src: 'maskable-icon-512x512.png',
+						src: 'android-chrome-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'maskable'
